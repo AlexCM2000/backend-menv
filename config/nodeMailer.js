e33d66623cf29f6fda30758d4003bfc649cf4da2
Alecs <alexchurata2000@gmail.com>
2026-05-02 21:39:20 -0400

    fix: corrigiendo remitente y puerto

--- config/nodeMailer.js
+++ config/nodeMailer.js
@@ -1,13 +1,10 @@
 import nodemailer from "nodemailer";
 
-export const createTransport =(host,port,user,pass)=>{
-
-    return  nodemailer.createTransport({
-        host ,
-        port,
-        auth: {
-          user,
-          pass,
-        },
-      });
+export const createTransport = (host, port, user, pass) => {
+  return nodemailer.createTransport({
+    host,
+    port: Number(port),
+    secure: false,
+    auth: { user, pass },
+  });
 }
